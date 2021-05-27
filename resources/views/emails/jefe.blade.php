d2355d0617f4f038db0ab15173934732e9054f59
Manuel <manuel.pp2209@gmail.com>
2021-05-26 19:09:14 -0500

    Tercer commit

--- resources/views/emails/jefe.blade.php
+++ resources/views/emails/jefe.blade.php
@@ -9,115 +9,61 @@
     </title>
 </head>
 
-<style>
-    #correo {
-        border: 1px solid #ccc;
-        box-shadow: 7px 7px 15px #592A08;
-        /* border-radius: 20px; */
-        width: 50%;
-        height: 100%;
-        text-align: center;
-        margin: 15px;
-    }
-
-    #link {
-        border: 2px solid #33A487;
-        border-radius: 4px;
-        width: 25%;
-        height: 24px;
-        text-align: center;
-        background-color: #3BB97A;
-        padding: 5px;
-        text-decoration: none !important;
-        color: white;
-        /* font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; */
-
-    }
-
-    #link-curso {
-        text-decoration: none;
-        color: white;
-        margin: 0;
-
-    }
-
-
-    #center {
-        text-align: center;
-    }
-
-    p {
-        padding: 20px;
-        margin-bottom: 10px;
-    }
-
-    #sup {
-        width: 100%;
-        padding: 0;
-        margin: 0;
-        height: 80%;
-        background-color: #52BE8E;
-    }
-
-    img {
-        width: 40%;
-        margin-top: 20px;
-        margin-bottom: 15px;
-        float: inline-end !important;
-    }
-
-</style>
 
 <body>
     {{-- <h3>{{ $curso }}</h3> --}}
-    <center>
-        <div id="correo">
-            <table>
-                <tr>
-                    <div id="sup">
-                    </div>
-                </tr>
-                {{-- <tr>
+    <div>
+
+        <center>
+            <div
+                style=" border: 1px solid #ccc; #592A08;width: 50%;height: 100%;text-align: center;margin: 15px">
+                <table>
+                    <tr>
+                        <div style="width: 100%;padding: 0;margin: 0;height: 80%;background-color: #52BE8E">
+                        </div>
+                    </tr>
+                    {{-- <tr>
                     <img src="https://www.pikpng.com/pngl/b/365-3652622_cfe-logo-comision-federal-de-electricidad-png-sport.png"
-                        alt="">
+                    alt="">
                 </tr> --}}
-                <tr>
+                    <tr>
 
 
-                    <h3>{{ $curso }}</h3>
+                        <h3>{{ $curso }}</h3>
 
-                    <h2>A continuacion se mostara los empleados con sus jefes correspondientes</h4>
+                        <h2>A continuacion se mostara los empleados con sus jefes correspondientes</h4>
 
-                        @foreach ($areas as $area)
-                            <h4>{{ $area->Nombre_Area }} -> {{ $area->Jefe_Area }}</h4>
-                            <br>
-                            @foreach ($empleados as $empleado)
-                                @if ($area->Clave_Area == $empleado->Clave_Area)
-                                    <li>{{ $empleado->Nombre_Empleado . ' ' . $empleado->ApellidoP_E . ' ' . $empleado->ApellidoM_E }}
-                                        ->
-                                        RPE : {{ $empleado->RPE_Empleado }}</li>
-                                @endif
+                            @foreach ($areas as $area)
+                                <h4>{{ $area->Nombre_Area }} -> {{ $area->Jefe_Area }}</h4>
+                                <br>
+                                @foreach ($empleados as $empleado)
+                                    @if ($area->Clave_Area == $empleado->Clave_Area)
+                                        <li>{{ $empleado->Nombre_Empleado . ' ' . $empleado->ApellidoP_E . ' ' . $empleado->ApellidoM_E }}
+                                            ->
+                                            RPE : {{ $empleado->RPE_Empleado }}</li>
+                                    @endif
+                                @endforeach
                             @endforeach
-                        @endforeach
-                        <br>
-                        <br>
+                            <br>
+                            <br>
 
-                        <center>
+                            <center>
 
-                            <div id="link">
-                                <a id="link-curso" href="{{ $links }}"> Encuesta Jefe Inmediato</a>
-                            </div>
-                        </center>
+                                <div style="border: 2px solid #33A487;border-radius: 4px;width: 25%;height: 24px;text-align: center;background-color: #3BB97A;padding: 5px;text-decoration: none !important; color: white">
+                                    <a style="text-decoration: none;color: white;margin: 0" href="{{ $links }}"> Encuesta Jefe Inmediato</a>
+                                </div>
+                            </center>
 
-                        <br>
+                            <br>
 
 
-                </tr>
-            </table>
+                    </tr>
+                </table>
 
-        </div>
+            </div>
 
-    </center>
+        </center>
 </body>
+</div>
 
 </html>
--- resources/views/emails/message.blade.php
+++ resources/views/emails/message.blade.php
@@ -3,113 +3,59 @@
     Fecha : Abril del 2021 -->
 
 
-<!DOCTYPE html>
-<html lang="en">
+ <!DOCTYPE html>
+ <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Encuesta de Reccion</title>
-    {{--
-    <link rel="stylesheet" href="{{ asset('css/email.css') }}"> --}}
-</head>
-<style>
-    #correo {
-        border: 1px solid #ccc;
-        box-shadow: 7px 7px 15px #592A08;
-        /* border-radius: 20px; */
-        width: 50%;
-        height: 100%;
-        text-align: center;
-        margin: 15px;
-    }
-
-    #link {
-        border: 2px solid #33A487;
-        border-radius: 4px;
-        width: 25%;
-        height: 24px;
-        text-align: center;
-        background-color: #3BB97A;
-        padding: 5px;
-        text-decoration: none !important;
-        color: white;
-        /* font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; */
-
-    }
-
-    #link-curso {
-        text-decoration: none;
-        color: white;
-
-    }
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Encuesta de Reccion</title>
+     {{-- <link rel="stylesheet" href="{{ asset('css/email.css') }}"> --}}
+ </head>
 
 
-    #center {
-        text-align: center;
-    }
+ <body>
+     <center>
 
-    p {
-        padding: 20px;
-        margin-bottom: 10px;
-    }
+         <div style="border: 1px solid #ccc; box-shadow: 7px 7px 15px #592A08; width: 50%; height: 100%; text-align: center; margin: 15px">
+             <table>
+                 <tr>
+                     <div style="width: 100%; padding: 0; margin: 0; height: 80%; background-color: #52BE8E;">
+                     </div>
+                 </tr>
+                 <tr>
+                     <img src="https://www.pikpng.com/pngl/b/365-3652622_cfe-logo-comision-federal-de-electricidad-png-sport.png"
+                         alt="">
+                 </tr>
+                 <tr>
 
-    #sup {
-        width: 100%;
-        padding: 0;
-        margin: 0;
-        height:80%;
-        background-color: #52BE8E;
-    }
-    img{
-        width: 40%;
-        margin-top: 20px;
-        margin-bottom: 15px;
-        float: inline-end !important;
-    }
-</style>
+                     <h2>Encuestas del Curso : </h2>
+                     <h3>{{ $curso }}</h3>
 
-<body>
-    <center>
+                     <p>
+                         Por este medio se le solicita al participante , que conteste las siguientes encuestas anexadas
+                         en el
+                         enlace que se encuentra en la parte inferior de este correo.
+                     </p>
+                     <center>
 
-        <div id="correo">
-            <table>
-                <tr>
-                    <div id="sup">
-                    </div>
-                </tr>
-                <tr>
-                    <img src="https://www.pikpng.com/pngl/b/365-3652622_cfe-logo-comision-federal-de-electricidad-png-sport.png" alt="">
-                </tr>
-                <tr>
+                         <div style="border: 2px solid #33A487;border-radius: 4px;width: 25%;height: 24px;text-align: center;background-color: #3BB97A;padding: 5px;text-decoration: none !important;color: white;">
+                             <a style="   text-decoration: none;color: white;" href="{{ $links }}"> Encuesta de Reaccion</a>
+                         </div>
+                     </center>
 
-                    <h2>Encuestas del Curso : </h2>
-                    <h3>{{$curso}}</h3>
-
-                    <p>
-                        Por este medio se le solicita al participante , que conteste las siguientes encuestas anexadas
-                        en el
-                        enlace que se encuentra en la parte inferior de este correo.
-                    </p>
-                    <center>
-
-                        <div id="link">
-                            <a id="link-curso" href="{{ $links }}"> Encuesta de Reaccion</a>
-                        </div>
-                    </center>
-
-                    <br>
+                     <br>
 
 
-                </tr>
-            </table>
+                 </tr>
+             </table>
 
-        </div>
-    </center>
+         </div>
+     </center>
 
 
 
-</body>
+ </body>
 
-</html>
+ </html>
